4dd6febe9badf7e6c1611e5faaf1c0a547b7348f
hyder12257 <hyder12257@gmail.com>
2023-01-30 23:39:38 +0500

    changes frontend

--- resources/views/templates/invester/plan.blade.php
+++ resources/views/templates/invester/plan.blade.php
@@ -5,7 +5,7 @@
             <div class="mb-4">
                 <div class="row mb-4">
                     <div class="col-lg-8">
-                        <h3 class="mb-2">@lang('Investment Plan')</h3>
+                        <h3 class="mb-2">@lang('Rented NFTs')</h3>
                     </div>
                 </div>
                 <div class="row gy-4">
